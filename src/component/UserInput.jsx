f545606a85d8bf80d37b1ff38730fe0392182820
geezyy <xgweshyx@gmail.com>
2023-06-07 13:35:26 +0100

    first commit

---
+++ src/component/UserInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserInput() {
+  return (
+    <div>
+        <input placeholder='Search....' className='texts' type="" />
+    </div>
+  )
+}
+
+export default UserInput
